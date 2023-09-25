921f38adcc978903cbf38881fbf0e2f0cb1747d1
chetan <jadhavchetan2506@gmail.com>
2023-09-25 13:54:46 +0530

    Add style in header and text in navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -19,22 +19,47 @@ const Navbar = () => {
             <ul>
                 <li><Link className='home' to="/">Home</Link></li>
                 <li onMouseEnter={handleServicesHover} onMouseLeave={handleDropdownLeave}>
-                    <Link to="/services">About</Link>
+                    <Link className='navlink' to="/services">About</Link>
                     {isDropdownVisible && (
                         <div className="dropdown">
-                            <Link to="/service1">Service 1</Link>
-                            <Link to="/service2">Service 2</Link>
+                            <Link className='dropdown-link' to="/service1">Institute</Link>
+                            <Link className='dropdown-link' to="/service2">Leadership</Link>
+                            <Link className='dropdown-link' to="/service2">Governing council</Link>
+                            <Link className='dropdown-link' to="/service2">Priciple</Link>
+                            <Link className='dropdown-link' to="/service2">Acadamic council</Link>
+                            <Link className='dropdown-link' to="/service2">Afiliations & Approvals</Link>
+                            <Link className='dropdown-link' to="/service2">Audit statements</Link>
                         </div>
                     )}
                 </li>
-                <li><Link to="/about">Academics</Link></li>
-                <li><Link to="/contact">Campus Life</Link></li>
-                <li><Link to="/contact">Administration</Link></li>
-                <li><Link to="/contact">Research</Link></li>
-                <li><Link to="/contact">Placements</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/contact">Links</Link></li>
-                <li><Link className='lms-icon' to="/contact"><FaArrowRight /></Link></li>
+                <li onMouseEnter={handleServicesHover} onMouseLeave={handleDropdownLeave}>
+                    <Link className='navlink' to="/about">Admission</Link>
+                    {isDropdownVisible && (
+                        <div className="dropdown">
+                            <Link className='dropdown-link' to="/service1">Admission procedures</Link>
+                            <Link className='dropdown-link' to="/service1">Apply online</Link>
+                            <Link className='dropdown-link' to="/service1">Application form</Link>
+                            <Link className='dropdown-link' to="/service1">Who can apply</Link>
+                        </div>
+                    )}
+                </li>
+                <li onMouseEnter={handleServicesHover} onMouseLeave={handleDropdownLeave} >
+                    <Link className='navlink' to="/contact">LMS</Link>
+                    {isDropdownVisible && (
+                        <div className="dropdown">
+                            <Link className='dropdown-link' to="/service1">Teacher managemant system</Link>
+                            <Link className='dropdown-link' to="/service1">Library managemant system</Link>
+                            <Link className='dropdown-link' to="/service1">Time table managemant system</Link>
+                        </div>
+                    )}
+                </li>
+                <li><Link className='navlink' to="/contact">Administration</Link></li>
+                <li><Link className='navlink' to="/contact">Expenses Management</Link></li>
+                <li><Link className='navlink' to="/contact">Research</Link></li>
+                <li><Link className='navlink' to="/contact">Placements</Link></li>
+                <li><Link className='navlink' to="/contact">Contact</Link></li>
+                <li><Link className='navlink' to="/contact">Links</Link></li>
+                <li><Link className='navlink' className='lms-icon' to="/contact"><FaArrowRight /></Link></li>
             </ul>
         </nav>
     );
